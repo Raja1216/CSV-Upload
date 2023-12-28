2811e89c0e4d5748ddc961b1e6f78ad4a756b862
Subhajit <subha.pramanik1212@gmail.com>
2023-12-28 23:22:56 +0530

    Code commited for port changes for host

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.get('/file/:filename', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
